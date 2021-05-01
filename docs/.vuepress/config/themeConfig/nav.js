f05165cc3f22e20b0a7f6b6f56b72a36d67cc734
Jeremy0820 <yujeremy0820@gmail.com>
2021-05-01 17:32:59 +0800

    third

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -15,7 +15,7 @@ module.exports = [
     ]
   },
   {
-    text: '前端',
+    text: 'FrontEnd',
     link: '/frontend/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
       // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
@@ -30,21 +30,57 @@ module.exports = [
     link: '/db/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
       // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
-      { text: 'MySQL', link: '' },
+      { text: 'MySQL', link: '/pages/779c4a/' },
       { text: 'Redis', link: '' },
       { text: 'MongoDB', link: '' }
     ],
   },
   {
-    text: '测试',
+    text: 'Testing',
     link: '/testing/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
       // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
-      { text: '测试基础理论', link: '/pages/123456781/' },
-      { text: '接口测试', link: '/pages/123456782/' },
-      { text: '性能测试', link: '/pages/123456783/' },
-      { text: 'UI自动化测试', link: '/pages/123456784/' },
-      { text: '测试开发', link: '/pages/123456785/' }
+      { text: '测试基础理论', link: '' },
+      { text: '接口测试', link: '' },
+      { text: '性能测试', link: '' },
+      { text: 'UI自动化测试', link: '' },
+      { text: '测试开发', link: '' }
+    ]
+  },
+  {
+    text: 'GoLang',
+    link: '/golang/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    items: [
+      // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
+      { text: '测试基础理论', link: '' },
+      { text: '接口测试', link: '' },
+      { text: '性能测试', link: '' },
+      { text: 'UI自动化测试', link: '' },
+      { text: '测试开发', link: '' }
+    ]
+  },
+  {
+    text: 'Java',
+    link: '/java/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    items: [
+      // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
+      { text: '测试基础理论', link: '' },
+      { text: '接口测试', link: '' },
+      { text: '性能测试', link: '' },
+      { text: 'UI自动化测试', link: '' },
+      { text: '测试开发', link: '' }
+    ]
+  },
+  {
+    text: 'Other',
+    link: '/other/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    items: [
+      // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
+      { text: '测试基础理论', link: '' },
+      { text: '接口测试', link: '' },
+      { text: '性能测试', link: '' },
+      { text: 'UI自动化测试', link: '' },
+      { text: '测试开发', link: '' }
     ]
   },
   { text: '关于我', link: '/about/' },
