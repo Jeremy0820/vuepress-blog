d4c0450a6e1b37a67975d61c62137bdc6bf7124c
Jeremy0820 <yujeremy0820@gmail.com>
2021-05-12 11:22:31 +0800

    512

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -7,7 +7,7 @@ module.exports = {
   nav,
   sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
   logo: '/img/EB-logo.png', // 导航栏logo
-  repo: 'xugaoyi/vuepress-theme-vdoing', // 导航栏右侧生成Github链接
+  repo: 'Jeremy0820/vuepress-blog', // 导航栏右侧生成Github链接
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
   docsDir: 'docs', // 编辑的文件夹
@@ -48,13 +48,13 @@ module.exports = {
   author: {
     // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, link: String}
     name: 'JeremyYu', // 必需
-    link: '', // 可选的  // https://github.com/xugaoyi
+    link: 'https://github.com/Jeremy0820', // 可选的  // https://github.com/xugaoyi
   },
   blogger: {
     // 博主信息，显示在首页侧边栏
     avatar: 'https://cdn.jsdelivr.net/gh/xugaoyi/image_store/blog/20200103123203.jpg',
     name: '于建财',
-    slogan: '技术小白中的小白',
+    slogan: '爱分享, 爱生活!',
   },
   social: {
     // 社交图标，显示于博主信息栏和页脚栏
@@ -68,7 +68,7 @@ module.exports = {
       {
         iconClass: 'icon-github',
         title: 'GitHub',
-        link: 'https://github.com/xugaoyi',
+        link: 'https://github.com/Jeremy0820/vuepress-blog',
       },
       {
         iconClass: 'icon-erji',
